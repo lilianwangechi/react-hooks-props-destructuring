73279cdae839957ea5447a987f732c27f9facb72
lilian <lilian.wangechi@student.moringaschool.com>
2022-09-28 12:49:01 +0300

    done

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,23 @@
 import React from "react";
 
+//without destructuring
+// function MovieCard(props){
+//   return ( 
+//     <div className="movie-card">
+//       <img src={props.posterSrc} alt={props.title} />
+//       <h2>{props.title}</h2>
+//       <small>{props.genres.join(", ")}</small>
+//     </div>
+//   );
+// }
+
+//with destructuring
 function MovieCard({
-  title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
-}) {
-  return (
+title,
+genres,
+posterSrc =  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM",
+}){
+  return ( 
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
       <h2>{title}</h2>
